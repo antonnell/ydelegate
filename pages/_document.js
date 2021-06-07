3626cbb0248b832956b6788b440928cfcccbe259
antonnell <antonnell.crpto@gmail.com>
2021-06-07 14:56:05 +0200

    inter lowercase might explain why the font isn't being picked up.
    Signed-off-by: antonnell <antonnell.crpto@gmail.com>

--- pages/_document.js
+++ pages/_document.js
@@ -8,7 +8,7 @@ export default class MyDocument extends Document {
     return (
       <Html lang="en">
         <Head>
-          <link rel="stylesheet" href="/fonts/Inter/Inter.css" />
+          <link rel="stylesheet" href="/fonts/Inter/inter.css" />
           <link rel="stylesheet" href="/fonts/Druk/Druk.css" />
         </Head>
         <body>
